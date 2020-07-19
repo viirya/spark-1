afd498e2ccf62205461bea2ab7cd93e94f3cc7aa
Liang-Chi Hsieh <viirya@gmail.com>
2020-07-19 16:13:41 -0700

    Revert.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -213,8 +213,7 @@ object SparkBuild extends PomBuild {
   )
 
   lazy val sharedSettings = sparkGenjavadocSettings ++
-      (if (sys.env.contains("NOLINT_ON_COMPILE")) Nil else enableScalaStyle) ++
-    (if (sys.env.contains("GITHUB_ACTIONS")) Seq(logLevel := Level.Error) else Nil)  ++ Seq(
+      (if (sys.env.contains("NOLINT_ON_COMPILE")) Nil else enableScalaStyle) ++ Seq(
     exportJars in Compile := true,
     exportJars in Test := false,
     javaHome := sys.env.get("JAVA_HOME")
