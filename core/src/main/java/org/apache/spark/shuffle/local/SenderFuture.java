55d75893adca28681fdbf640e484d5e138e74b67
Liang-Chi Hsieh <viirya@gmail.com>
2025-03-19 00:40:04 -0700

    more

---
+++ core/src/main/java/org/apache/spark/shuffle/local/SenderFuture.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.shuffle.local;
+
+import java.util.concurrent.CompletableFuture;
+
+class SenderFuture<T> {
+    private final T data;
+    private final Waker waker;
+    private final Channel<T> channel;
+
+    SenderFuture(T data, Channel<T> channel) {
+        this.data = data;
+        this.waker = new SimpleWaker();
+        this.channel = channel;
+    }
+
+    Waker getWaker() {
+        return waker;
+    }
+
+    void wake() {
+        waker.wake();
+    }
+
+    CompletableFuture<T> getFuture() {
+        CompletableFuture<T> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.println("Task Paused...");
+                waker.await();
+
+                boolean isEmpty = channel.isEmpty();
+                channel.addData(data);
+
+                // If data queue was empty before pushing new data, wake up the receivers
+                if (isEmpty) {
+                    channel.wakeReceivers();
+                }
+            } catch (InterruptedException e) {
+                return data;
+            }
+            return data;
+        });
+
+        return future.get();
+    }
+}
