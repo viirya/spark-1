dbad9962ddb1615676e4e2f17c63cc4448dd05ca
Liang-Chi Hsieh <viirya@gmail.com>
2025-07-26 10:55:11 -0700

    Improve UnionExec outputPartitioning

--- sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/BroadcastExchangeExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/BroadcastExchangeExec.scala
@@ -145,12 +145,6 @@ case class BroadcastExchangeExec(
     Statistics(dataSize, Some(rowCount))
   }
 
-  override def resetMetrics(): Unit = {
-    // no-op
-    // BroadcastExchangeExec after materialized won't be materialized again, so we should not
-    // reset the metrics. Otherwise, we will lose the metrics collected in the broadcast job.
-  }
-
   @transient
   private lazy val promise = Promise[broadcast.Broadcast[Any]]()
 
